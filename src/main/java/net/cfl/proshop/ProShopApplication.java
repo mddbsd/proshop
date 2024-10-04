6d7afb7c76195f2e8b17d10a03f0e413687a78fa
max <mdd.bsd@gmail.com>
2024-10-04 10:30:57 -0300

    Commit inicial

---
+++ src/main/java/net/cfl/proshop/ProShopApplication.java
@@ -0,0 +1,13 @@
+package net.cfl.proshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProShopApplication.class, args);
+	}
+
+}
