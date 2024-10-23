d74cf742210a68aff9007656a3d755182ed658ef
max <mdd.bsd@gmail.com>
2024-10-23 09:02:25 -0300

    implementado producto controlador

--- src/main/java/net/cfl/proshop/controlador/ProductoControlador.java
+++ src/main/java/net/cfl/proshop/controlador/ProductoControlador.java
@@ -2,13 +2,22 @@ package net.cfl.proshop.controlador;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import net.cfl.proshop.excepciones.RecursoNoEncontradoEx;
 import net.cfl.proshop.modelo.Producto;
+import net.cfl.proshop.request.ActualizaProductoReq;
+import net.cfl.proshop.request.AgregaProductoReq;
 import net.cfl.proshop.respuesta.ApiRespuesta;
 import net.cfl.proshop.servicios.categoria.ICategoriaServicio;
 import net.cfl.proshop.servicios.producto.IProductoServicio;
@@ -24,11 +33,146 @@ public class ProductoControlador {
 		List<Producto> productos = productoServicio.listarProductos();
 		return ResponseEntity.ok(new ApiRespuesta("Exito!", productos));
 	}
-	
-	public ResponseEntity<ApiRespuesta> listarProductoPorId(Long productoId){
-		Producto producto = productoServicio.listaProductoPorId(productoId);
-		return ResponseEntity.ok(new ApiRespuesta("Exito!", producto));
+	@GetMapping("/producto/{productoId}/producto")
+	public ResponseEntity<ApiRespuesta> listarProductoPorId(@PathVariable Long productoId){
+		try {
+			Producto producto = productoServicio.listaProductoPorId(productoId);
+			return ResponseEntity.ok(new ApiRespuesta("Exito!", producto));
+		} catch (RecursoNoEncontradoEx e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND)
+					.body(new ApiRespuesta(e.getMessage(), null));
+		}
+	}
+	@PostMapping("/agrega")
+	public ResponseEntity<ApiRespuesta> agregarProducto(@RequestBody AgregaProductoReq producto){
+		try {
+			Producto elProducto = productoServicio.agregaProducto(producto);
+			return ResponseEntity.ok(new ApiRespuesta("Producto agregado!", elProducto));
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(new ApiRespuesta(e.getMessage(), null));
+		}
+	}
+	@PutMapping("/producto/{productoId}/actualiza")
+	public ResponseEntity<ApiRespuesta> actualizaProducto(@RequestBody ActualizaProductoReq request, @PathVariable Long idProducto){
+		try {
+			Producto elProducto = productoServicio.actualizaProducto(request, idProducto);
+			return ResponseEntity.ok(new ApiRespuesta("Producto Actualizado!", elProducto));
+		} catch (RecursoNoEncontradoEx e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND)
+					.body(new ApiRespuesta(e.getMessage(), null));
+		}
+	}
+	@DeleteMapping("/producto/{productoId}/borrar")
+	public ResponseEntity<ApiRespuesta> borrarProducto (@PathVariable Long productoId){
+		try {
+			productoServicio.borrarProducto(productoId);
+			return ResponseEntity.ok(new ApiRespuesta("Producto Borrado!", null));
+		} catch (RecursoNoEncontradoEx e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND)
+					.body(new ApiRespuesta(e.getMessage(), null));
+		}
+	}
+	@GetMapping("/por/marca-y-nombre")
+	public ResponseEntity<ApiRespuesta> listarPorMarcaYNombre(@RequestBody String marcaNombre, @RequestBody String productoNombre) {
+		try {
+			List<Producto> productos = productoServicio.listarPorNombreYMarca(productoNombre, marcaNombre);
+			if(productos.isEmpty()){
+				return ResponseEntity.status(HttpStatus.NOT_FOUND)
+						.body(new ApiRespuesta("No se encontraron productos", null));
+			}
+			return ResponseEntity.ok(new ApiRespuesta("Exito", productos));
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(new ApiRespuesta(e.getMessage(), null));
+		}
 	}
 	
+	@GetMapping("/por/marca-y-categoria")
+	public ResponseEntity<ApiRespuesta> listarPorMarcaYCategoria(@RequestBody String marcaNombre, @RequestBody String categoriaNombre) {
+		try {
+			List<Producto> productos = productoServicio.listarPorMarcaYCategoria(marcaNombre, categoriaNombre);
+			if(productos.isEmpty()){
+				return ResponseEntity.status(HttpStatus.NOT_FOUND)
+						.body(new ApiRespuesta("No se encontraron productos", null));
+			}
+			return ResponseEntity.ok(new ApiRespuesta("Exito", productos));
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(new ApiRespuesta(e.getMessage(), null));
+		}
+	}
 	
+	@GetMapping("/producto/{productoNombre}/productos")
+	public ResponseEntity<ApiRespuesta> listarPorNombre(@PathVariable String productoNombre) {
+		try {
+			List<Producto> productos = productoServicio.listarPorNombre(productoNombre);
+			if(productos.isEmpty()){
+				return ResponseEntity.status(HttpStatus.NOT_FOUND)
+						.body(new ApiRespuesta("No se encontraron productos", null));
+			}
+			return ResponseEntity.ok(new ApiRespuesta("Exito", productos));
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(new ApiRespuesta(e.getMessage(), null));
+		}
+	}
+	
+	@GetMapping("/por/marca")
+	public ResponseEntity<ApiRespuesta> listarPorMarca(@RequestBody String marcaNombre) {
+		try {
+			List<Producto> productos = productoServicio.listarPorMarca(marcaNombre);
+			if(productos.isEmpty()){
+				return ResponseEntity.status(HttpStatus.NOT_FOUND)
+						.body(new ApiRespuesta("No se encontraron productos", null));
+			}
+			return ResponseEntity.ok(new ApiRespuesta("Exito", productos));
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(new ApiRespuesta(e.getMessage(), null));
+		}
+	}
+	
+	@GetMapping("/producto/{productoCategoria}/todos/productos")
+	public ResponseEntity<ApiRespuesta> listarCategoria(@PathVariable String categoriaNombre) {
+		try {
+			List<Producto> productos = productoServicio.listarPorCategoria(categoriaNombre);
+			if(productos.isEmpty()){
+				return ResponseEntity.status(HttpStatus.NOT_FOUND)
+						.body(new ApiRespuesta("No se encontraron productos", null));
+			}
+			return ResponseEntity.ok(new ApiRespuesta("Exito", productos));
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(new ApiRespuesta(e.getMessage(), null));
+		}
+	}
+	
+	@GetMapping("/producto/cuenta/por/marca-y-nombre")
+	public ResponseEntity<ApiRespuesta> contarPorMarcaYNombre(@RequestBody String marcaNombre, @RequestBody String categoriaNombre) {
+		try {
+			//var crea una variable sin especificar el tipo de dato
+			//El tipo de variable se define al momento de ingresar el dato
+			var productoCuenta = productoServicio.contarProductosPorNombreYMarca(marcaNombre, categoriaNombre);
+			return ResponseEntity.ok(new ApiRespuesta("Exito", productoCuenta));
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(new ApiRespuesta(e.getMessage(), null));
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
